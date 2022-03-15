37b52466251615ce9820659c4c5e0dd278623948
Michael <michael@msbuilds.io>
2022-03-15 10:12:52 -0400

    added blog to the menu

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -12,6 +12,15 @@
             </a>
             <div class="flex items-center text-white bg-black shadow-[3px_3px_rgba(0,0,0,0.15)]">
                 <a class="flex items-center justify-center px-4 py-2 transition-all border-4 border-transparent hover:underline hover:text-yellow-400 hover:decoration-1 focus:text-yellow-400 focus:underline focus:decoration-1 focus:outline-none group"
+                    href="/blog"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="inline w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <title>Blog</title>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+                    </svg>
+                    <span class="hidden md:inline">Blog</span>
+                </a>
+                <a class="flex items-center justify-center px-4 py-2 transition-all border-4 border-transparent hover:underline hover:text-yellow-400 hover:decoration-1 focus:text-yellow-400 focus:underline focus:decoration-1 focus:outline-none group"
                     href="https://www.twitter.com/msbuildsio"
                 >
                     <svg role="img" class="inline w-5 h-5 mr-1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
