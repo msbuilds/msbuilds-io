2ecf628f694c0f5eeb1e08147307a95d65338d96
Michael <michael@msbuilds.io>
2022-03-16 15:56:43 -0400

    nord theme for code

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
     content: {
         markdown: {
             prism: {
-                theme: 'prism-themes/themes/prism-material-oceanic.css'
+                theme: 'prism-themes/themes/prism-nord.css'
             }
         }
     }
